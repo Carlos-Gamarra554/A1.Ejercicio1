625abfc9fbb2f654438be8d543a1070d14cb4252
Carlos Gamarra Tomás <cargamtom@alu.edu.gva.es>
2025-01-21 09:41:53 +0100

    Delete src/Vehiculo.java

--- src/Vehiculo.java
+++
@@ -1,9 +0,0 @@
-public class Vehiculo {
-    private int ruedas;
-    private String color;
-
-    public Vehiculo(int ruedas, String color) {
-        this.ruedas = ruedas;
-        this.color = color;
-    }
-}
