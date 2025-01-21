187b84edb9d2f3b8eb54b8eb82ffa1f5e6ea832f
Carlos-Gamarra554 <cargamtom@alu.edu.gva.es>
2025-01-21 09:37:33 +0100

    Cilindro

---
+++ src/MainCilindro.java
@@ -0,0 +1,7 @@
+public class MainCilindro {
+    public static void main(String[] args) {
+        Cilindro cilindro = new Cilindro(5,15.4);
+
+        System.out.println("El volumen del cilindro es de: " + Cilindro.getVolume() + " cm2");
+    }
+}
